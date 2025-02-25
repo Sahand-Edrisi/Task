dea6b07465c7861991af0ac92c84c82789489dfa
sahandedc <sahandedc@gmail.com>
2025-02-25 15:03:58 +0330

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import CardItem from "./Component/CardItem";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const response = axios.get("https://rotikala.chbk.app/products/4/");
+    response
+      .then((response) => {
+        if (response.status === 200) {
+          setProduct(response.data.data);
+        }
+      })
+      .catch((e) => {
+        console.log(e.massage);
+      });
+  }, []);
   return (
     <div className="text-center w-[100%] flex justify-center mt-[100px]">
-      <CardItem />
+      {product.product !== undefined ? <CardItem product={product.product} /> : undefined}
     </div>
   );
 }
--- src/Component/CardItem.jsx
+++ src/Component/CardItem.jsx
@@ -1,47 +1,35 @@
-import React, { useState } from "react";
 import { Card } from "flowbite-react";
-import Product from "../Data/Data";
-import bagImage from "../Data/img/bag-img.avif";
 
-function CardItem() {
-  const [product] = useState(Product);
-
+function CardItem({ product }) {
+  console.log(product);
   return (
     <div className="cardContainer">
-      {product.map((product, index) => (
-        <Card key={index} className="max-w-sm p-3">
-          <img
-            src={bagImage}
-            alt=""
-            className="w-[100%] h-[300px] object-cover "
-          />
-          <a href=" #">
-            <h5 className="text-[25px] font-bold tracking-tight text-right text-gray-600 dark:text-white">
-              {product.Name}
-            </h5>
-            <h5 className="text-[25px] font-semibold tracking-tight text-right text-gray-600 dark:text-white">
-              {product.Model}
-            </h5>
-          </a>
-          <span className="text-[28px] text-left h-4 font-bold text-gray-500 dark:text-white ">
-            <div className="h-1 w-[100px] mt-[19px] bg-red-600 absolute z-10 "></div>
-            {product.price}
-          </span>
-          <div className="flex items-center justify-between ">
-            <div className="flex ">
-              <span className="text-[28px] font-bold text-green-400 dark:text-white pr-[5px]">
-                {product.MonetaryUnit}
-              </span>
-              <span className="text-[30px] font-bold text-green-400 dark:text-white ">
-                {product.DiscountedPrice}
-              </span>
-            </div>
-            <span className="text-[20px] bg-red-500 px-2 ml-[20px] text-gray-300 font-bold rounded-xl">
-              {product.DiscountPercentage}
+      <Card className="max-w-sm p-3">
+        <img
+          src={product.images[0].image}
+          alt=""
+          className="w-[100%] h-[300px] object-cover "
+        />
+        <p className="text-[19px] font-bold tracking-tight text-right text-gray-600 dark:text-white">
+          {product.nameFa}
+        </p>
+        <p className="text-[19px] font-bold tracking-tight text-right text-gray-600 dark:text-white">
+        <span > رنگ :</span> {product.color[0].color}
+        </p>
+        <span className="text-[28px] text-left h-4 font-bold text-gray-500 dark:text-white ">
+          {product.price}
+        </span>
+        <div className="flex items-center justify-between ">
+          <div className="flex ">
+            <span className="text-[28px] font-bold text-green-400 dark:text-white pr-[5px]">
+              {product.MonetaryUnit}
             </span>
           </div>
-        </Card>
-      ))}
+          <span className="text-[20px] bg-red-500 px-2 ml-[20px] text-gray-300 font-bold rounded-xl">
+            {product.DiscountPercentage}
+          </span>
+        </div>
+      </Card>
     </div>
   );
 }
