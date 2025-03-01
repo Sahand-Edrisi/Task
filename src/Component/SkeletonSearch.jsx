5905c3b2c5b26e8edef41fb7c778ce8ae11fcb29
sahandedc <sahandedc@gmail.com>
2025-03-01 16:24:30 +0330

    debug

--- src/Component/SkeletonSearch.jsx
+++ src/Component/SkeletonSearch.jsx
@@ -4,8 +4,8 @@ function SkeletonSearch() {
   return (
     <div role="status" class=" mb-[50px] space-y-2.5 animate-pulse max-w-lg">
       <div class="flex items-center w-full">
-        <div class="h-[30px] w-[250px] ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-        <div class="h-[30px] w-[80px] ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+        <div class="h-[30px] w-[250px] ms-2 bg-gray-300 rounded-full dark:bg-gray-600 "></div>
+        <div class="h-[30px] w-[80px] ms-2 bg-gray-300 rounded-full dark:bg-gray-600 "></div>
       </div>
       <span class="sr-only">Loading...</span>
     </div>
